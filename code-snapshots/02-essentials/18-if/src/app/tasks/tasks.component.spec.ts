47be791da681db1fe2ca3ee3165154fe29c671dc
Parasmani Jain <parasmani.jain2208@gmail.com>
2026-03-29 10:17:47 +0530

    ts 6 migration updated

---
+++ code-snapshots/02-essentials/18-if/src/app/tasks/tasks.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TasksComponent } from './tasks.component';
+
+describe('TasksComponent', () => {
+  let component: TasksComponent;
+  let fixture: ComponentFixture<TasksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TasksComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TasksComponent);
+    component = fixture.componentInstance;
+    
+    // Set required input properties
+    component.name = 'Test User';
+    
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ code-snapshots/02-essentials/18-if/src/app/user/user.component.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserComponent } from './user.component';
+
+describe('UserComponent', () => {
+  let component: UserComponent;
+  let fixture: ComponentFixture<UserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UserComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UserComponent);
+    component = fixture.componentInstance;
+    
+    // Set required input properties
+    component.user = {
+      id: 'u1',
+      name: 'Test User',
+      avatar: 'test-avatar.jpg'
+    };
+    
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
