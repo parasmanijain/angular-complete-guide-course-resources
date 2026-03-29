a704c36ea99fb32eed9b274f0853856fed5e7e16
Parasmani Jain <parasmani.jain2208@gmail.com>
2026-03-29 11:43:06 +0530

    ts6 migration done

---
+++ code-snapshots/02-essentials/25-styling-conditional-classes/src/app/tasks/task/task.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TaskComponent } from './task.component';
+import { Task } from './task.model';
+
+describe('TaskComponent', () => {
+  let component: TaskComponent;
+  let fixture: ComponentFixture<TaskComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TaskComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TaskComponent);
+    component = fixture.componentInstance;
+    
+    // Set required inputs
+    component.task = { id: 't1', userId: 'u1', title: 'Test Task', summary: 'Test Summary', dueDate: '2024-01-01' } as Task;
+    
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
