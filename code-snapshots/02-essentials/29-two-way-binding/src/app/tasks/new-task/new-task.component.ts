62aba5af3425a20c861bf728ccbd96297b6134e6
Parasmani Jain <parasmani.jain2208@gmail.com>
2026-03-29 12:30:49 +0530

    ts 6 migration done

--- code-snapshots/02-essentials/29-two-way-binding/src/app/tasks/new-task/new-task.component.ts
+++ code-snapshots/02-essentials/29-two-way-binding/src/app/tasks/new-task/new-task.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
+  standalone: true,
   imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.scss',
---
+++ code-snapshots/02-essentials/29-two-way-binding/src/app/tasks/task/task.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TaskComponent } from './task.component';
+
+describe('TaskComponent', () => {
+  let component: TaskComponent;
+  let fixture: ComponentFixture<TaskComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TaskComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TaskComponent);
+    component = fixture.componentInstance;
+    // Set required input properties
+    component.task = {
+      id: 't1',
+      userId: 'u1',
+      title: 'Test Task',
+      summary: 'Test Summary',
+      dueDate: '2024-01-01'
+    };
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
